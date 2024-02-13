294fb2e88abef69cc2907035bb0b3e1c6ac13189
David Gaspar <davsangas@hotmail.com>
2024-02-12 18:59:31 -0600

    Update navbar component HTML and TypeScript files

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
 import { OverlayModule } from '@angular/cdk/overlay';
+
+interface WorkSpaces {
+  img?: string;
+  title: string;
+  subtitle: string;
+  isHovered?: boolean;
+}
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -9,4 +16,47 @@ import { OverlayModule } from '@angular/cdk/overlay';
 })
 export class NavbarComponent {
   isOpen = false
+  isWorkSpaceOpen = false
+  isRecentOpen = false
+  isMarketOpen = false
+  isTemplatesOpen = false
+
+  workSpaces: WorkSpaces[] = [
+    {
+      img: '',
+      title: '6to semestre',
+      subtitle: 'Ingeniería en sistemas',
+      isHovered: false
+    },
+    {
+      img: '',
+      title: '5to semestre',
+      subtitle: 'Ingeniería en sistemas',
+      isHovered: false
+    },
+    {
+      img: '',
+      title: '4to semestre',
+      subtitle: 'Ingeniería en sistemas',
+      isHovered: false
+    },
+    {
+      img: '',
+      title: '3er semestre',
+      subtitle: 'Ingeniería en sistemas',
+      isHovered: false
+    },
+    {
+      img: '',
+      title: '2do semestre',
+      subtitle: 'Ingeniería en sistemas',
+      isHovered: false
+    },
+    {
+      img: '',
+      title: '1er semestre',
+      subtitle: 'Ingeniería en sistemas',
+      isHovered: false
+    },
+  ]
 }
