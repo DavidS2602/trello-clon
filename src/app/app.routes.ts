91aa07ec4b8d007996e505791ba0a334b5cdfa28
David <DavidS260202@proton.me>
2024-04-29 20:31:19 -0600

    Fix w-full input

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import type { Routes } from '@angular/router';
 import { LayoutComponent } from './pages/shared/layout/layout.component';
 
 export const routes: Routes = [
