e2902da428bd794ad6eb5840347ccad076802371
DavidS2602 <DavidS260202@proton.me>
2024-06-02 14:45:03 -0600

    Update: auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,45 @@
 import type { Routes } from '@angular/router';
-import { LayoutComponent } from './pages/shared/layout/layout.component';
+import { LayoutComponent } from './auth/pages/layout/layout.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    component: LayoutComponent,
+    children: [
+      {
         path: '',
-        component: LayoutComponent,
-        children: [
-            {
-                path: '',
-                loadComponent: () => import('./pages/login/login.component')
-            },
-            {
-                path: 'reset-password',
-                loadComponent: () => import('./pages/reset-password/reset-password.component')
-            },
-            {
-                path: 'sign-up',
-                loadComponent: () => import('./pages/sign-up/sign-up.component')
-            }
-        ],
-    },
-    {
-        path: 'boards',
-        loadComponent: () => import('./pages/boards/boards.component')
-    },
-    {
-        path: 'board',
-        loadComponent: () => import('./pages/board/board.component')
-    }
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/pages/login/login.component')
+      },
+      {
+        path: 'reset-password',
+        loadComponent: () => import('./pages/reset-password/reset-password.component')
+      },
+      {
+        path: 'sign-up',
+        loadComponent: () => import('./auth/pages/sign-up/sign-up.component')
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ],
+  },
+  {
+    path: 'boards',
+    loadComponent: () => import('./pages/boards/boards.component')
+  },
+  {
+    path: 'board',
+    loadComponent: () => import('./pages/board/board.component')
+  }
 ];
