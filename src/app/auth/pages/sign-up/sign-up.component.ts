179b188a1a359492dfa0d2c019cdef345490210a
DavidS2602 <DavidS260202@proton.me>
2024-06-05 08:12:59 -0600

    Add register functionality

--- src/app/auth/pages/sign-up/sign-up.component.ts
+++ src/app/auth/pages/sign-up/sign-up.component.ts
@@ -1,5 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLinkWithHref } from '@angular/router';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { Router, RouterLinkWithHref } from '@angular/router';
+import { AuthService } from '@app/auth/services/auth.service';
+import { RequestStatus } from '@app/auth/interfaces/request-status';
+
+
+interface InputField {
+  name: string;
+  type: string;
+  placeholder: string;
+}
 
 interface SignUp {
   title: string;
@@ -10,26 +21,121 @@ interface SignUp {
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, ReactiveFormsModule, CommonModule],
   templateUrl: './sign-up.component.html'
 })
 export default class SignUpComponent {
+
+  //*Rules validation
+  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  //*Checking if the password and confirmPassword are the same
+  public isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const fieldValue1 = formGroup.get(field1)?.value;
+      const fieldValue2 = formGroup.get(field2)?.value;
+      if (fieldValue1 !== fieldValue2) {
+        formGroup.get(field2)?.setErrors({ notEqual: true });
+        return { notEqual: true }
+      }
+      formGroup.get(field2)?.setErrors(null);
+      return null;
+    }
+  }
+
+  //!Add dependencies
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  public registerForm = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(this.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', Validators.required]
+  }, {
+    validators: [
+      this.isFieldOneEqualFieldTwo('password', 'confirmPassword')
+    ]
+  });
+
+  status: RequestStatus = 'init';
+
+
+  getErrorMessage(field: string): string {
+    const control = this.registerForm.get(field);
+    if (control?.hasError('required')) {
+      return 'Este campo es obligatorio';
+    }
+    if (control?.hasError('pattern')) {
+      if (field === 'email') {
+        return 'Correo no válido';
+      }
+      if (field === 'name') {
+        return 'Nombre no válido';
+      }
+    }
+    if (control?.hasError('minlength')) {
+      const requiredLength = control.errors?.['minlength'].requiredLength;
+      return `La contraseña debe tener al menos ${requiredLength} caracteres`;
+    }
+    if (control?.hasError('notEqual')) {
+      return 'Las contraseñas no coinciden';
+    }
+    return '';
+  }
+
+  //*Render register inputs
+  inputFields: InputField[] = [
+    { name: 'name', type: 'text', placeholder: 'Introduce tu nombre' },
+    { name: 'email', type: 'email', placeholder: 'Introduce tu correo' },
+    { name: 'password', type: 'password', placeholder: 'Introduce tu contraseña' },
+    { name: 'confirmPassword', type: 'password', placeholder: 'Confirma tu contraseña' }
+  ];
+
+  register() {
+    if (this.registerForm.valid) {
+      this.status = 'loading';
+      const formValue = this.registerForm.value;
+      const name = formValue.name as string;
+      const email = formValue.email as string;
+      const password = formValue.password as string;
+      if (name && email && password) {
+        this.authService.register(name, email, password)
+        .subscribe({
+          next: () => {
+            this.status = 'success';
+            this.router.navigateByUrl('/auth/login');
+            console.log('Register success')
+          },
+          error: (error) => {
+            console.error('Register error:', error);
+            this.status = 'failed';
+          }
+        });
+      }
+    }
+  }
+
   SignUp: SignUp[] = [
     {
       title: 'Google',
-      src:'/assets/Icons/icons8-google.svg'
+      src: '/assets/Icons/icons8-google.svg'
     },
     {
       title: 'Microsoft',
-      src:'/assets/Icons/icons8-microsoft.svg'
+      src: '/assets/Icons/icons8-microsoft.svg'
     },
     {
       title: 'Apple',
-      src:'/assets/Icons/icons8-apple.svg'
+      src: '/assets/Icons/icons8-apple.svg'
     },
     {
       title: 'Slack',
-      src:'/assets/Icons/icons8-slack.svg'
+      src: '/assets/Icons/icons8-slack.svg'
     },
   ]
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -16,5 +16,12 @@ export class AuthService {
       password
     });
   }
+  register(name: string, email: string, password: string) {
+    return this.httpClient.post(`${this.apiUrl}/api/v1/auth/register`, {
+      name,
+      email,
+      password
+    });
+  }
 
 }
