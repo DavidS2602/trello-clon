4e108452a76b6347e30e6117c26aa53ca4612fb2
DavidS2602 <DavidS260202@proton.me>
2024-06-08 17:54:01 -0600

    Add: expire session guard

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,8 +1,10 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { TokenService } from '@app/services/token.service';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
   const tokenService = inject(TokenService);
   const router = inject(Router)
 
@@ -12,6 +14,15 @@ export const authGuard: CanActivateFn = (route, state) => {
     return false;
   }
 
+  authService.getProfile().subscribe(
+    () => {},
+    (error) => {
+      if (error.status === 401) {
+        tokenService.removeToken();
+        router.navigateByUrl('/auth/login');
+      }
+    }
+  )
 
   return true;
 };
